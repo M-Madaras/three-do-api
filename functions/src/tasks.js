1aa336f31bf7c475bde951b42da5223da0272612
“Mason <masonmadaras@gmail.com>
2022-08-26 10:50:22 -0400

    added get and post task

--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,10 +1,29 @@
-export function getTasks(req, res){
-    res.send('TASKS');
+import dbConnect from "./dbConnect.js";
+
+export async function getTasks(req, res){
+    const db = dbConnect();
+     const collection = await db.collection('tasks').get()
+     .catch(err => res.status(500).send(err));
+     const tasks = collection.docs.map(doc => {
+        // return {...doc.data(), id: doc.id};
+        let task = doc.data();
+        task.id = doc.id;
+        return task;
+     })
+    res.send(tasks);
 }
 
-export function createTask(req,res) {
+export async function createTask(req,res) { // will add user id and a time stamp...
     const newTask = req.body;
-    res.status(201).send('Task Added');
+    if (!newTask || !newTask.task) {
+        res.status(400).send({ success: false, message: 'Invalid request'});
+        return;
+    } 
+    const db = dbConnect();
+    await db.collection('tasks').add(newTask)
+       .catch(err => res.status(500).send(err));
+    res.status(201);
+    getTasks(req, res); // send back full list of task
 }
 
 export function updateTask(req, res) {
